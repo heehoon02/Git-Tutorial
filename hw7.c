9dcd3f329a9ab2c23b4e8f6917407170556efea7
heehoon02 <heehoon02@naver.com>
2021-11-23 23:55:31 +0900

    Add project [hw7.c]

---
+++ hw7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+double pow(double a, double b);
+double sqrt(double x);
+
+int main(void)
+{
+	int i;
+	int arr[5] = { 1, 2, 3, 4, 5 };
+
+	for (i = 0; i < 5; i++)
+		arr(i) = sacn
+}
